e9c6c8d5e1877d6e0cc709a3b2f6d07253612b45
muzahid604 <604muzahid@gmail.com>
2022-05-13 18:03:02 +0600

    appointment loaded

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000
 const cors = require('cors');
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongoConnection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.opu6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const appointmentCollection = client.db("health-blomm").collection('appointment')
+
+        app.get('/appointment', async (req, res) => {
+            const query = {};
+            const cursor = appointmentCollection.find(query);
+            const appointment = await cursor.toArray();
+            res.send(appointment);
+        })
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
